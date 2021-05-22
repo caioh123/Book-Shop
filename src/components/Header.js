e22b2233f807a426efb24032a152c678a426ae1a
Caio Henrique <caionwebdev@gmail.com>
2021-05-22 14:51:15 -0300

    fetching data from the API, some style changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-
+import React, { useState } from "react";
+import "react-toastify/dist/ReactToastify.css";
 import "../App.css";
 import {
   InputGroup,
@@ -10,54 +10,87 @@ import {
   Label,
 } from "reactstrap";
 import { AiOutlineSearch } from "react-icons/ai";
+import { ToastContainer, toast } from "react-toastify";
+import axios from "axios";
 
 const Header = () => {
   const [maxResults, setMaxResults] = useState(10);
   const [query, setQuery] = useState("");
   const [loading, setLoading] = useState(false);
+  const [startIndex, setStartIndex] = useState(1);
 
   const handleSubmit = () => {
     setLoading(true);
+    if (maxResults > 40 || maxResults < 1) {
+      toast.error("max results must be between 1 and 40");
+    } else {
+      axios
+        .get(
+          `https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=${maxResults}&${startIndex}`
+        )
+        .then((response) => {
+          if (startIndex >= response.data.totalItems || startIndex < 1) {
+            toast.error(
+              `max results must be between 1 and ${response.data.totalItems}`
+            );
+          }
+          console.log(response.data);
+        })
+        .catch((err) => console.log(err));
+    }
   };
 
   return (
-    <div className="main-image d-flex justify-content-center align-items-center flex-column">
-      <div className="filter"></div>
-      <h1 className="display-2 text-center text-white mb-3 filter-title">
-        Riachuelo books
-      </h1>
-      <div style={{ width: "60%", zIndex: 2 }}>
-        <InputGroup size="lg" className="mb-3">
-          <Input
-            placeholder="Book Search"
-            value={query}
-            onChange={(event) => setQuery(event.target.value)}
-          />
-          <InputGroupAddon addonType="append">
-            <Button
-              style={{ height: 50 }}
-              color="secondary"
-              onClick={handleSubmit}
-            >
-              <AiOutlineSearch size={20} />
-            </Button>
-          </InputGroupAddon>
-        </InputGroup>
-        <div className="d-flex text-white justify-content-center">
-          <FormGroup className="d-flex justify-content-center flex-column align-items-center ml-5">
-            <Label
-              style={{ fontSize: 20 }}
-              for="maxResults"
-              value={maxResults}
-              onChange={(event) => setMaxResults(event.target.value)}
-            >
-              Max Results
-            </Label>
-            <Input type="number" id="maxResults" placeholder="Max Results" />
-          </FormGroup>
+    <>
+      <ToastContainer />
+
+      <div className="main-image d-flex justify-content-center align-items-center flex-column">
+        <div className="filter"></div>
+        <h1 className="display-2 text-center text-white mb-3 filter-title">
+          Riachuelo books
+        </h1>
+        <div style={{ width: "60%", zIndex: 2 }}>
+          <InputGroup size="lg" className="mb-3">
+            <Input
+              placeholder="Book Search"
+              value={query}
+              onChange={(event) => setQuery(event.target.value)}
+            />
+            <InputGroupAddon addonType="append">
+              <Button
+                style={{ height: 50 }}
+                color="secondary"
+                onClick={handleSubmit}
+              >
+                <AiOutlineSearch size={20} />
+              </Button>
+            </InputGroupAddon>
+          </InputGroup>
+          <div className="d-flex text-white justify-content-center">
+            <FormGroup style={{ marginRight: 50 }}>
+              <Label for="maxResults">Max Results</Label>
+              <Input
+                type="number"
+                id="maxResults"
+                placeholder="Max Results"
+                value={maxResults}
+                onChange={(e) => setMaxResults(e.target.value)}
+              />
+            </FormGroup>
+            <FormGroup className="ml-5">
+              <Label for="startIndex">Start Index</Label>
+              <Input
+                type="number"
+                id="startIndex"
+                placeholder="Start Index"
+                value={startIndex}
+                onChange={(e) => setStartIndex(e.target.value)}
+              />
+            </FormGroup>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
